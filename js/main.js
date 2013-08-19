73bf7b12fa62377f329b4495a4fdc7fecca5dead
Thierry Blais <thierryblais@hydra.net>
2013-08-19 15:08:21 -0400

    page style g draft

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ $(document).on('mobileinit', function(e){
 		});
 	
 $(document).on('keyup', function(){
-	$.mobile.changePage('#kraemer_cancer_center');
+	$.mobile.changePage('#st_josephs_community_foundation');
 	});	
 
 
