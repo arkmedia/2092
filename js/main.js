36961ac0ab3103fe9b213c4675624474d00cc65e
Thierry Blais <thierryblais@hydra.net>
2013-08-22 22:06:26 -0400

    Revert "Revert "changed donors jQ to pure JS""
    This reverts commit 72a613b45afaad81eb4e27b847a111a112ea08c8.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,28 @@
 // JavaScript Document
-var footerRetracted = false,
-	movingMenuPosition = 1;
-
+var footerRetracted = false;
 $(document).on('mobileinit', function(e){
-	
-	/* resize footerNavSubMenuContainer to accomodate menu */
-	var footerNavSubMenuContainer = $('.footer-nav-sub-menu-container');
-	footerNavSubMenuContainer.width(function(){
-		var l = footerNavSubMenuContainer.find('td').length * 100;
-		return l + '%';
+		var donorGridButtons = document.querySelectorAll('.page_style_d .donor-grid li'),
+			donorGridButtonsLength = donorGridButtons.length,
+			lists =  document.querySelectorAll('.donor-list-container ul'),
+			listsLength = lists.length;
+		
+		for (i=0;i<donorGridButtonsLength;i++){
+			donorGridButtons[i].addEventListener('touchstart mousedown', function(){
+				for (h=0;h<donorGridButtonsLength;h++){
+					donorGridButtons[h].classList.remove('donor-active');
+					}
+				this.classList.add('donor-active');
+				for (i=0;h<listsLength;h++){
+					lists[h].classList.remove('default');
+					lists[h].style.display = 'none';
+					}	
+				document.getElementById(this.getAttribute('data-for')).style.display = 'inline-block';									
+				});
+			}
 		});
+	
 
-	
-		});
-	
+
 
 
 $('.page_style_d .donor-grid li')	.on('touchstart mousedown', function(){
@@ -48,30 +57,31 @@ $('#front_page')	.on('pagehide', function(event){
 							}						
 						});
 						
-$('a.footer-btn').on('touchstart mousedown', function(){
+/*$('a.footer-btn').on('touchstart mousedown', function(){
 	var movingMenu = $('.footer-nav-sub-menu-container'),
 		numberOfMovingMenuItems = movingMenu.find('td.footer-nav-sub-menu-item').length,
 		destination = this.getAttribute('href'),
 		destinationIndex = parseInt(destination.replace( /^\D+/g, ''), 10);
 	
 	/* move sub menu */	
-	movingMenu.animate({
+/*	movingMenu.animate({
 		left: '-' + $(destination).position().left
 		}, 300,function(){
 			movingMenuPosition = destinationIndex;
 			/* change destinations */
-			$('.footer-btn').each(function(index, element){
+	/*		$('.footer-btn').each(function(index, element){
 				if (element.classList.contains('footer-prev-btn')){
 					element.setAttribute('href', '#sub_menu_' + (movingMenuPosition-1));
 					/* disabled state */
-					element.style.visibility = ((movingMenuPosition-1) < 1) ? 'hidden' : 'visible' ;
+		/*			element.style.visibility = ((movingMenuPosition-1) < 1) ? 'hidden' : 'visible' ;
 					} else if (element.classList.contains('footer-next-btn')){
 						element.setAttribute('href', '#sub_menu_' + (movingMenuPosition+1));
 						/* disabled state */
-						element.style.visibility = ((movingMenuPosition+1) > numberOfMovingMenuItems) ? 'hidden' : 'visible' ;
+			/*			element.style.visibility = ((movingMenuPosition+1) > numberOfMovingMenuItems) ? 'hidden' : 'visible' ;
 						}									
 				});
 
 			})
 	return false;
 	})
+*/
